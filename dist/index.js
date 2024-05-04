e177e1dc67cf36c491d914ca2a31bd6150b7c0f0
Kei Ikeda <kikeda@falconstreehouse.com>
2024-05-04 07:11:09 -0400

    Add getTags function

--- dist/index.js
+++ dist/index.js
@@ -31088,7 +31088,7 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(8021);
 const github = __nccwpck_require__(4366);
-const { exec } = __nccwpck_require__(2081);
+const { execSync } = __nccwpck_require__(2081);
 
 const tagFormat = core.getInput('tag-format');
 
@@ -31117,12 +31117,45 @@ function generateRegexPattern(formatString) {
                        .replace('${rev}', '\\d+');
 }
 
+/**
+ * Fetches git tags and filters them based on a provided regex pattern. If no pattern is provided,
+ * it returns all git tags.
+ * @param {string} [pattern] The optional regex pattern to filter the tags.
+ * @returns {string[]} An array of tags that match the given pattern, or all tags if no pattern is provided.
+ */
+function getTags(pattern) {
+    try {
+        // Fetch all tags from remote to ensure the local repository is up to date
+        execSync('git fetch --tags');
+
+        // Get all tags from the Git repository and split them into an array
+        const tags = execSync('git tag').toString().split('\n').filter(Boolean);
+
+        // If no pattern is provided, return all tags
+        if (!pattern) {
+            return tags;
+        }
+
+        // Create a RegExp object from the provided pattern
+        const regex = new RegExp(pattern);
+
+        // Filter the tags based on the regex pattern
+        return tags.filter(tag => regex.test(tag));
+    } catch (error) {
+        // Handle and log any errors that occur during execution
+        console.error(`Failed to fetch or filter tags: ${error.message}`);
+        return [];  // Return an empty array in case of error
+    }
+}
 
 try {
 
     // Generate regex pattern to be used to filter tags
     const regexPattern = generateRegexPattern(tagFormat);
     console.log('\x1b[33m%s\x1b[0m', `Regex pattern: ${regexPattern}`);
+
+    tags = getTags(regexPattern);
+    console.log('\x1b[33m%s\x1b[0m', `Tags: ${tags}`);
 
 } catch (error) {
     core.setFailed(error.message);

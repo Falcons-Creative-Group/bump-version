264024d1ee6bf6a190b73136f76d025ba5c4e53e
Kei Ikeda <kikeda@falconstreehouse.com>
2024-05-04 06:55:35 -0400

    Add missing compiled index.js

--- dist/index.js
+++ dist/index.js
@@ -31088,7 +31088,12 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(8021);
 const github = __nccwpck_require__(4366);
-const { execSync } = __nccwpck_require__(2081);
+const { exec } = __nccwpck_require__(2081);
+
+const tagFormat = core.getInput('tag-format');
+
+// Log tag format.
+console.log('\x1b[33m%s\x1b[0m', `Tag format: ${tagFormat}`);
 
 // List all existing tags.
 // Filter tags that match the current app's pattern.
@@ -31097,31 +31102,28 @@ const { execSync } = __nccwpck_require__(2081);
 // Increment the version based on your versioning scheme (e.g., semantic versioning).
 
 /**
- *
- * Fetches all git tags from the repository and returns them as an array.
- * @returns {string[]} An array of git tags.
+ * Generate regex pattern to be used to filter tags.
+ * @param {string} formatString.
+ * @returns {string} regex pattern to be used to filter tags.
  */
-function getTags() {
-    return execSync('git fetch --tags && git tag').toString().split('\n').filter(Boolean);
+function generateRegexPattern(formatString) {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // +1 because getMonth() is zero-indexed
+
+    // Replace placeholders with actual year and month
+    return formatString.replace('${year}', year)
+                       .replace('${month}', month)
+                       .replace('${rev}', '\\d+');
 }
 
 
 try {
-    // Get the 'tag-format' input from the workflow file.
-    const tagFormat = core.getInput('tag-format');
 
-    try {
-        // Get all tags from the repository.
-        const tags = getTags();
+    // Generate regex pattern to be used to filter tags
+    const regexPattern = generateRegexPattern(tagFormat);
+    console.log('\x1b[33m%s\x1b[0m', `Regex pattern: ${regexPattern}`);
 
-        if (tags.length > 0) {
-            tags.forEach(tag => console.log(tag));
-        } else {
-            console.log('No tags found.');
-        }
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-    }
 } catch (error) {
     core.setFailed(error.message);
 }

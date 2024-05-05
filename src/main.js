e2f18e176936648152e1730993e681f61c46e0f1
Kei Ikeda <kikeda@falconstreehouse.com>
2024-05-05 19:58:42 -0400

    Make 2-indent spacing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const fs = require('fs');
 const { execSync } = require('child_process');
 
@@ -10,13 +9,13 @@ const { execSync } = require('child_process');
  * @returns {string} Resolved tag format.
  */
 function resolveTagFormat(tagFormat, versionObject) {
-    const currentDate = new Date();
-    const year = versionObject?.year || currentDate.getFullYear();
-    let month = versionObject?.month || currentDate.getMonth() + 1; // +1 because getMonth() is zero-indexed
-    month = String(month).padStart(2, '0'); // Ensure month is always two digits
+  const currentDate = new Date();
+  const year = versionObject?.year || currentDate.getFullYear();
+  let month = versionObject?.month || currentDate.getMonth() + 1; // +1 because getMonth() is zero-indexed
+  month = String(month).padStart(2, '0'); // Ensure month is always two digits
 
-    // Replace placeholders with actual year and month
-    return tagFormat.replace('${year}', year).replace('${month}', month);
+  // Replace placeholders with actual year and month
+  return tagFormat.replace('${year}', year).replace('${month}', month);
 }
 
 /**
@@ -27,27 +26,27 @@ function resolveTagFormat(tagFormat, versionObject) {
  */
 function getTags(pattern) {
 
-    try {
-        // Fetch all tags from remote to ensure the local repository is up to date
-        execSync('git fetch --tags');
+  try {
+    // Fetch all tags from remote to ensure the local repository is up to date
+    execSync('git fetch --tags');
 
-        // Get all tags from the Git repository and split them into an array
-        const tags = execSync('git tag').toString().split('\n').filter(Boolean);
+    // Get all tags from the Git repository and split them into an array
+    const tags = execSync('git tag').toString().split('\n').filter(Boolean);
 
-        // If no pattern is provided, return all tags
-        if (!pattern) {
-            return tags;
-        }
-
-        // Create a RegExp object from the provided pattern
-        const regex = new RegExp(pattern);
-
-        // Filter the tags based on the regex pattern
-        return tags.filter(tag => regex.test(tag));
-    } catch (error) {
-        console.error(`Failed to fetch or filter tags: ${error.message}`);
-        return [];
+    // If no pattern is provided, return all tags
+    if (!pattern) {
+      return tags;
     }
+
+    // Create a RegExp object from the provided pattern
+    const regex = new RegExp(pattern);
+
+    // Filter the tags based on the regex pattern
+    return tags.filter(tag => regex.test(tag));
+  } catch (error) {
+    console.error(`Failed to fetch or filter tags: ${error.message}`);
+    return [];
+  }
 }
 
 /**
@@ -58,27 +57,27 @@ function getTags(pattern) {
  * @returns {string} The next version of the tag with the incremented revision number or with the revision number set to 1 if no tags are present.
  */
 function getNextVersion(tagArray, tagFormat) {
-    // Create a regular expression to match the revision part of the tag based on the provided format
-    // Replace the ${rev} placeholder with a regex group to capture revision numbers (\d+)
-    // Replace all '.' characters with '\.' to escape them in the regex.
-    const revisionRegex = new RegExp(tagFormat.replace('${rev}', '(\\d+)').replace(/\./g, '\\.'));
+  // Create a regular expression to match the revision part of the tag based on the provided format
+  // Replace the ${rev} placeholder with a regex group to capture revision numbers (\d+)
+  // Replace all '.' characters with '\.' to escape them in the regex.
+  const revisionRegex = new RegExp(tagFormat.replace('${rev}', '(\\d+)').replace(/\./g, '\\.'));
 
-    let highestRevision = 0;
+  let highestRevision = 0;
 
-    tagArray.forEach(tag => {
-        const match = tag.match(revisionRegex);
-        if (match) {
-            // Parse the revision number and compare it to the current highest number
-            const revisionNumber = parseInt(match[1], 10);
-            if (revisionNumber > highestRevision) {
-                highestRevision = revisionNumber;
-            }
-        }
-    });
+  tagArray.forEach(tag => {
+    const match = tag.match(revisionRegex);
+    if (match) {
+      // Parse the revision number and compare it to the current highest number
+      const revisionNumber = parseInt(match[1], 10);
+      if (revisionNumber > highestRevision) {
+        highestRevision = revisionNumber;
+      }
+    }
+  });
 
-    const nextRevisionNumber = highestRevision + 1;
+  const nextRevisionNumber = highestRevision + 1;
 
-    return tagFormat.replace('${rev}', nextRevisionNumber);
+  return tagFormat.replace('${rev}', nextRevisionNumber);
 }
 
 /**
@@ -86,40 +85,40 @@ function getNextVersion(tagArray, tagFormat) {
  * @returns {Promise<void>} A promise that resolves when the action has completed.
  */
 async function run() {
-    try {
-        const tagFormat = core.getInput('tag-format');
-        const versionFile = core.getInput('version-file');
+  try {
+    const tagFormat = core.getInput('tag-format');
+    const versionFile = core.getInput('version-file');
 
-        let versionObject;
+    let versionObject;
 
-        if (versionFile && fs.existsSync(versionFile)) {
-            const versionString = fs.readFileSync(versionFile, 'utf8');
-            versionObject = JSON.parse(versionString);
-            console.log(`Version file content: ${JSON.stringify(versionObject)}`);
-        }
-        const resolvedTagFormat = resolveTagFormat(tagFormat, versionObject);
-
-				// Generate regex pattern to be used to filter tags
-				const pattern = resolvedTagFormat.replace('${rev}', '\\d+');
-				console.log(`Regex pattern: ${pattern}`);
-
-        const tags = getTags(pattern);
-        console.log(`Tags: ${tags}`);
-
-        const nextVersion = getNextVersion(tags, resolvedTagFormat);
-        console.log(`Next version: ${nextVersion}`);
-
-        core.setOutput('version', nextVersion);
-
-        // Output the payload for debugging
-        core.info(
-            `The event payload: ${JSON.stringify(github.context.payload, null, 2)}`
-        )
-    } catch (error) {
-        core.setFailed(error.message);
+    if (versionFile && fs.existsSync(versionFile)) {
+      const versionString = fs.readFileSync(versionFile, 'utf8');
+      versionObject = JSON.parse(versionString);
+      console.log(`Version file content: ${JSON.stringify(versionObject)}`);
     }
+    const resolvedTagFormat = resolveTagFormat(tagFormat, versionObject);
+
+    // Generate regex pattern to be used to filter tags
+    const pattern = resolvedTagFormat.replace('${rev}', '\\d+');
+    console.log(`Regex pattern: ${pattern}`);
+
+    const tags = getTags(pattern);
+    console.log(`Tags: ${tags}`);
+
+    const nextVersion = getNextVersion(tags, resolvedTagFormat);
+    console.log(`Next version: ${nextVersion}`);
+
+    core.setOutput('version', nextVersion);
+
+    // Output the payload for debugging
+    core.info(
+      `The event payload: ${JSON.stringify(github.context.payload, null, 2)}`
+    )
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 module.exports = {
-    run
-}
+  run
+};

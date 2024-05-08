616982f1b16e951944c878d00617f1a2680e6972
kikeda-falcons <129531855+kikeda-falcons@users.noreply.github.com>
2024-05-08 13:36:34 -0400

    PIP-381 Implement functionality of bump version (#1)
    * Print tag format
    
    * Add action/core module
    
    * Add license
    
    * Add built index.js
    
    * Fix typo
    
    * Try to node 16
    
    * Revert to node20
    
    * Fix module missing issue
    
    * Add actions/github module
    
    * Use the new dist/index file
    
    * Add compailed index.js
    
    * List git tags
    
    * Add function to generate regex pattern
    
    * Add missing compiled index.js
    
    * Add getTags function
    
    * Add function to get next version
    
    * Get revision numbers
    
    * Refactor
    
    * Fix regex
    
    * Set output for next version
    
    * Fix undefined tagFormat
    
    * Add src folder and async run function
    
    * Add version-file as input
    
    * Respect data from version file
    
    * Update comment
    
    * Refactor
    
    * Make 2-indent spacing
    
    * Add back missing github module
    
    * Print out revision regex
    
    * Update README.md
    
    * Use {rev} not {revision}
    
    * Add functionality test on resolveTagFormat
    
    * Add support for release candidate
    
    * Rename to releaseCandidate
    
    * Update Readme.md
    
    * Update README.md
    
    * Remove unecessary bunch of code

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# bump-version
-GitHub Action for automated version bump for internal monorepo
+# Git Tag Versioning Action
+
+This GitHub Action automates the process of managing version tags in your Git repository. It resolves tag formats, fetches and filters tags, and calculates the next version based on revision numbers.
+
+## Inputs
+
+### `tag-format`
+
+Optional. The tag format string that may contain placeholders for dynamic elements. The current supported placeholders are `${rev}`, `${year}`, and `${month}`. You can include a long prefix such as `${year}.${month}-sg.tk-config-default2.1.4.5.${rev}` to create complex tag structures tailored to your project's needs. Example: `v${rev}`. This input is not required, and if not provided, the default format used will be `v${rev}`, which increments the revision number with each new tag.
+
+### `version-file`
+
+Optional. The path to a JSON file containing custom values for 'year' and 'month'. If provided, these values will be used to replace placeholders in the tag format. If `version-file` is not provided, the action will default to using the current year and month for the placeholders.
+
+### `release-candidate`
+
+Optional. A flag that indicates whether the tag being generated is for a release candidate. Accepts a boolean value (`true` or `false`). When set to `true`, the action will handle versioning to include a release candidate suffix (`-rc1`). This is useful for pre-release versions where you may need additional testing or validation before the final release.
+
+## Outputs
+
+### `version`
+
+The calculated next version of the tag based on the provided tag format and existing tags in the repository.
+
+## Usage
+
+```yaml
+name: Versioning
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  is-prerelease:
+    name: Determine if this is a prerelease
+    uses: Falcons-Creative-Group/ci-cd-workflows/.github/workflows/prerelease-check.yml@main
+
+  release:
+    name: Release
+    needs: is-prerelease
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v4
+        with:
+          fetch-depth: 0
+
+      - name: Bump version
+        id: version-increment
+        uses: Falcons-Creative-Group/bump-version@main
+        with:
+          tag-format: '${year}.${month}-sg.tk-config-default2.1.4.5.${rev}'
+          version-file: 'version.json'
+          release-candidate: ${{ fromJSON(needs.is-prerelease.outputs.is-prerelease) }}
+
+      - name: Print out new version
+        run: echo ${{ steps.version-increment.outputs.version }}
+```
+
+## Building
+
+To build the action before deploying or testing, sue the following steps:
+
+1. Install `@vercel/ncc` by running:
+
+```
+npm install -g @vercel/ncc
+```
+This will install ncc globally on your machine, allowing you to use it for any project.
+
+2. Navigate to the root directory of your project and run:
+
+```
+ncc build ./src/index.js
+```
+This command compiles the source code located in ./src/index.js into a single minified JavaScript file in the dist directory. This file includes all dependencies, which are necessary for the action to run in GitHub's environment.
+
+## Testing
+
+1. Ensure Jest is installed in your project. If it's not installed, you can install it by running:
+
+```
+npm install --save-dev jest
+```
+
+2. To run the tests, use the following command:
+
+```
+npx jest --verbose
+```
---
+++ src/main.js
@@ -0,0 +1,143 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+const { execSync } = require('child_process');
+const { reverse } = require('dns');
+
+/**
+ * Resolves the tag format by replacing placeholders with actual year and month values.
+ * @param {string} tagFormat The tag format string that may contain placeholders for 'year' and 'month'.
+ * @param {Object} versionObject Optional JSON object containing custom values for 'year' and 'month'.
+ * @returns {string} Resolved tag format.
+ */
+function resolveTagFormat(tagFormat, versionObject) {
+  const currentDate = new Date();
+  const year = versionObject?.year || currentDate.getFullYear();
+  let month = versionObject?.month || currentDate.getMonth() + 1; // +1 because getMonth() is zero-indexed
+  month = String(month).padStart(2, '0'); // Ensure month is always two digits
+
+  // Replace placeholders with actual year and month
+  return tagFormat.replace('${year}', year).replace('${month}', month);
+}
+
+/**
+ * Fetches git tags and filters them based on a provided regex pattern. If no pattern is provided,
+ * it returns all git tags.
+ * @param {string} pattern The optional regex pattern to filter the tags.
+ * @returns {string[]} An array of tags that match the given pattern, or all tags if no pattern is provided.
+ */
+function getTags(pattern) {
+
+  try {
+    // Fetch all tags from remote to ensure the local repository is up to date
+    execSync('git fetch --tags');
+
+    // Get all tags from the Git repository and split them into an array
+    const tags = execSync('git tag').toString().split('\n').filter(Boolean);
+
+    // If no pattern is provided, return all tags
+    if (!pattern) {
+      return tags;
+    }
+
+    // Create a RegExp object from the provided pattern
+    const regex = new RegExp(pattern);
+
+    // Filter the tags based on the regex pattern
+    return tags.filter(tag => regex.test(tag));
+  } catch (error) {
+    console.error(`Failed to fetch or filter tags: ${error.message}`);
+    return [];
+  }
+}
+
+/**
+ * Extracts and increments the highest revision number from a list of version tags.
+ * Handles release candidate versions if specified.
+ * Starts from revision number 1 if no valid tags are found.
+ *
+ * @param {string[]} tagArray An array of version tag strings.
+ * @param {string} tagFormat A format string for the tag, including a `${rev}` placeholder for the revision number.
+ * @param {boolean} [releaseCandidate=false] Flag indicating whether the tag is for a release candidate.
+ * @returns {string} The next version of the tag, with either the incremented revision number or set to 1 if no tags are present.
+ */
+function getNextVersion(tagArray, tagFormat, releaseCandidate = false) {
+  // Create a regex to extract the revision number from the tag. Escape dots and replace `${rev}` with a capture group for digits.
+	const revisionRegex = new RegExp(`${tagFormat.replace('${rev}', '(\\d+)').replace(/\./g, '\\.')}$`);
+
+  let highestRevision = 0;
+
+  // Find the highest revision number in the existing tags.
+  tagArray.forEach(tag => {
+    const match = tag.match(revisionRegex);
+    if (match) {
+      highestRevision = Math.max(highestRevision, parseInt(match[1], 10));
+    }
+  });
+
+  // Increment the revision number for the next tag.
+  const nextRevisionNumber = highestRevision + 1;
+  let nextVersionTag = tagFormat.replace('${rev}', nextRevisionNumber);
+
+  if (releaseCandidate) {
+    // Handle release candidate versions by finding and incrementing the highest RC number.
+    const rcRegex = new RegExp(`${nextVersionTag}-rc(\\d+)`);
+    const rcVersions = tagArray.filter(tag => rcRegex.test(tag));
+
+    const highestRC = rcVersions.reduce((max, tag) => {
+      const rcMatch = tag.match(rcRegex);
+      return Math.max(max, parseInt(rcMatch[1], 10));
+    }, 0);
+
+    // Append incremented RC version to the next tag.
+    nextVersionTag += `-rc${highestRC + 1}`;
+  }
+
+  return nextVersionTag;
+}
+
+/**
+ * Main function that orchestrates the execution of the action.
+ * @returns {Promise<void>} A promise that resolves when the action has completed.
+ */
+async function run() {
+  try {
+    const tagFormat = core.getInput('tag-format');
+    const versionFile = core.getInput('version-file');
+		const releaseCandidate = core.getInput('release-candidate') === 'true';
+
+    let versionObject;
+
+    if (versionFile && fs.existsSync(versionFile)) {
+      const versionString = fs.readFileSync(versionFile, 'utf8');
+      versionObject = JSON.parse(versionString);
+      console.log(`Version file content: ${JSON.stringify(versionObject)}`);
+    }
+    const resolvedTagFormat = resolveTagFormat(tagFormat, versionObject);
+
+    // Generate regex pattern to be used to filter tags
+    const pattern = resolvedTagFormat.replace('${rev}', '\\d+');
+    console.log(`Regex pattern: ${pattern}`);
+
+    const tags = getTags(pattern);
+    console.log(`Tags: ${tags}`);
+
+    const nextVersion = getNextVersion(tags, resolvedTagFormat, releaseCandidate);
+    console.log(`Next version: ${nextVersion}`);
+
+    core.setOutput('version', nextVersion);
+
+    // Output the payload for debugging
+    core.info(
+      `The event payload: ${JSON.stringify(github.context.payload, null, 2)}`
+    )
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = {
+	resolveTagFormat,
+	getNextVersion,
+  run
+};
